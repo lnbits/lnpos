9c738d4c463317f34a4d22436078ab4d69ed8c65
dni ⚡ <office@dnilabs.com>
2024-07-30 23:33:46 +0200

    fix build

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 command -v arduino-cli >/dev/null 2>&1 || { echo >&2 "arduino-cli not found. Aborting."; exit 1; }
-arduino-cli config --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json init
+arduino-cli config --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json init --overwrite
 arduino-cli core update-index
-arduino-cli core install esp32:esp32
 arduino-cli upgrade
+# uBitcoin is broken on esp32 3.x.x
+arduino-cli core install esp32:esp32@2.0.17
 arduino-cli lib install ArduinoJson Base64 Keypad uBitcoin
 arduino-cli compile \
     --build-property "build.partitions=min_spiffs" \
