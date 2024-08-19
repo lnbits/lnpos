33146b86fd69c2b82a17f4c7319adc8deeae4cba
arcbtc <ben@arc.wales>
2024-08-19 20:58:19 +0100

    Auto stash before merge of "main" and "origin/main"

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ arduino-cli config --additional-urls https://raw.githubusercontent.com/espressif
 arduino-cli core update-index
 arduino-cli upgrade
 # uBitcoin is broken on esp32 3.x.x
-arduino-cli core install esp32:esp32@3.0.4
+arduino-cli core install esp32:esp32@2.0.17
 arduino-cli lib install ArduinoJson Base64 Keypad uBitcoin
 arduino-cli compile \
     --build-property "build.partitions=min_spiffs" \
