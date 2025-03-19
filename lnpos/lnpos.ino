5fb35a74eb094efd419e2fb610b3e1f076c44257
dni ⚡ <office@dnilabs.com>
2025-03-19 10:14:41 +0100

    change urls

--- lnpos/lnpos.ino
+++ lnpos/lnpos.ino
@@ -34,11 +34,11 @@ bool format = false;
 
 bool hardcoded = false; /// Set to true to hardcode
 
-String lnurlPoS = "https://legend.lnbits.com/lnpos/api/v1/lnurl/WTmei,BzzoY5wbgpym3eMdb9ueXr,USD";
-String lnurlATM = "https://legend.lnbits.com/fossa/api/v1/lnurl/atm/W5xu4,XGg4BJ3xCh36JdMKm2kgDw,USD";
+String lnurlPoS = "https://demo.lnbits.com/lnpos/api/v1/lnurl/WTmei,BzzoY5wbgpym3eMdb9ueXr,USD";
+String lnurlATM = "https://demo.lnbits.com/fossa/api/v1/lnurl/atm/W5xu4,XGg4BJ3xCh36JdMKm2kgDw,USD";
 String masterKey = "xpub6CJFgwcim8tPBJo2A6dS13kZxqbgtWKD3LKj1tyurWADbXbPyWo11exyotTSUY3cvhQy5Mfj8FSURgpXhc4L2UvQyaTMC36S49JnNJMmcWU";
-String lnbitsServer = "https://legend.lnbits.com";
-String invoice = "37d45d3e1f0d4572a905bad544588d7d";
+String lnbitsServer = "https://demo.lnbits.com";
+String invoice = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 String lncurrency = "GBP";
 String lnurlATMMS = "https://mempool.space";
 String lnurlATMPin = "878787";
