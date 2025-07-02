9e99214ecb0ee55a31527e8daa959ad7a72efe86
dni ⚡ <office@dnilabs.com>
2025-07-02 09:11:25 +0200

    feat: change aes to be compatible with lnbits utils/crypto.py (#25)
    * feat: change aes to be compatible with lnbits utils/crypto.py

--- lnpos/lnpos.ino
+++ lnpos/lnpos.ino
@@ -10,6 +10,7 @@ fs::SPIFFSFS &FlashFS = SPIFFS;
 #include "qrcoded.h"
 #include <WiFiClientSecure.h>
 #include "mbedtls/aes.h"
+#include "mbedtls/md5.h"
 
 // ArduinoJson, Keypad and uBitcoin should be installed using the Arduino Library Manager.
 // The latest versions should work, verified with ArduinoJson 7.2.1, Keypad 3.1.1 and uBitcoin 0.2.0
@@ -18,7 +19,7 @@ fs::SPIFFSFS &FlashFS = SPIFFS;
 #include <Keypad.h>
 #include <ArduinoJson.h>
 
-#define VERSION "0.1.4"
+#define VERSION "1.0.0"
 #define PARAM_FILE "/elements.json"
 #define KEY_FILE "/thekey.txt"
 #define USB_POWER 1000 // battery percentage sentinel value to indicate USB power
@@ -35,7 +36,7 @@ bool format = false;
 bool hardcoded = false; /// Set to true to hardcode
 
 String lnurlPoS = "https://demo.lnbits.com/lnpos/api/v1/lnurl/WTmei,BzzoY5wbgpym3eMdb9ueXr,USD";
-String lnurlATM = "https://demo.lnbits.com/fossa/api/v1/lnurl/atm/W5xu4,XGg4BJ3xCh36JdMKm2kgDw,USD";
+String lnurlATM = "https://demo.lnbits.com/fossa/api/v1/lnurl/W5xu4,XGg4BJ3xCh36JdMKm2kgDw,USD";
 String masterKey = "xpub6CJFgwcim8tPBJo2A6dS13kZxqbgtWKD3LKj1tyurWADbXbPyWo11exyotTSUY3cvhQy5Mfj8FSURgpXhc4L2UvQyaTMC36S49JnNJMmcWU";
 String lnbitsServer = "https://demo.lnbits.com";
 String invoice = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
@@ -1600,16 +1601,8 @@ bool makeLNURL()
 
   float total = amountToShow.toFloat() * multipler;
 
-  int iv_length = 16;
-  unsigned char iv_init[iv_length];
-  unsigned char iv[iv_length];
-
-  for (int i = 0; i < iv_length; i++) {
-      iv[i] = random(0, 255);
-      iv_init[i] = iv[i];
-  }
-
   String secret;
+  char hexbuffer[3];
 
   if (selection == "Offline PoS") {
     preparedURL = baseURLPoS;
@@ -1620,20 +1613,46 @@ bool makeLNURL()
     secret = secretATM;
   }
 
+  int salt_length = 8;
+  unsigned char salt[salt_length];
+
+  for (int i = 0; i < salt_length; i++) {
+    salt[i] = random(0, 256);
+  }
+
+  unsigned char keyIV[32 + 16] = {0};
+  deriveKeyAndIV(secret.c_str(), salt, keyIV);
+
+  unsigned char key[32] = {0};
+  unsigned char iv[16] = {0};
+
+  memcpy(key, keyIV, 32);
+  memcpy(iv, keyIV + 32, 16);
+
   randomPin = random(1000, 9999);
   String payload = String(randomPin) + String(":") + String(total);
+
   Serial.print("payload: ");
   Serial.println(payload);
+
   size_t payload_len = payload.length();
   int padding = 16 - (payload_len % 16);
   payload_len += padding;
+  for (int i = 0; i < padding; i++) {
+    payload += String((char)padding);
+  }
+
   unsigned char encrypted[payload_len] = {0};
-  encrypt(secret.c_str(), iv, payload_len, payload.c_str(), encrypted);
+  encrypt(key, iv, payload_len, payload.c_str(), encrypted);
+
+  const unsigned char *saltedChars = (const unsigned char *)"Salted__";
+  unsigned char salted[payload_len + 16];
+  memcpy(salted, saltedChars, 8);
+  memcpy(salted + 8, salt, salt_length);
+  memcpy(salted + 16, encrypted, payload_len);
 
   preparedURL += "?p=";
-  preparedURL += toBase64(encrypted, payload_len, BASE64_URLSAFE | BASE64_NOPADDING);
-  preparedURL += "&iv=";
-  preparedURL += toBase64(iv_init, iv_length, BASE64_URLSAFE | BASE64_NOPADDING);
+  preparedURL += toBase64(salted, payload_len+16, BASE64_URLSAFE);
   Serial.println(preparedURL);
 
   char Buf[200];
@@ -1869,14 +1888,42 @@ void printSleepAnimationFrame(String text, int wait)
 }
 
 //////////ENCRYPTION///////////////
-void encrypt(const char* key, unsigned char* iv, int length, const char* plainText, unsigned char* outputBuffer){
-  if (strlen(key) != 16) {
-    Serial.println("Key must be 16 bytes long. not " + String(strlen(key)) + " bytes.");
-    return;
-  }
+void encrypt(unsigned char* key, unsigned char* iv, int length, const char* plainText, unsigned char* outputBuffer){
   mbedtls_aes_context aes;
   mbedtls_aes_init(&aes);
-  mbedtls_aes_setkey_enc(&aes, (const unsigned char*)key, strlen(key)*8);
+  mbedtls_aes_setkey_enc(&aes, key, 256); // AES-256 requires a 32-byte key
   mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, length, iv, (const unsigned char*)plainText, outputBuffer);
   mbedtls_aes_free(&aes);
 }
+
+void deriveKeyAndIV(const char* secret, unsigned char* salt, unsigned char* outputBuffer) {
+  mbedtls_md5_context md5_ctx;
+  unsigned char data[24]; // 16 bytes key + 8 bytes salt
+  unsigned char md5Output[16]; // 16 bytes for MD5 output
+
+  memcpy(data, secret, 16);
+  memcpy(data + 16, salt, 8);
+
+  // first iteration
+  mbedtls_md5_init(&md5_ctx);
+  mbedtls_md5_update(&md5_ctx, data, sizeof(data));
+  mbedtls_md5_finish(&md5_ctx, md5Output);
+
+  // Copy the first 16 bytes to the output buffer for the key
+  memcpy(outputBuffer, md5Output, 16);
+
+  unsigned char data_md5[16 + 16 + 8]; // 16 bytes md5 output + 16 bytes key + 8 bytes salt
+
+  for (int i = 16; i <= 48; i+=16) {
+    memcpy(data_md5, md5Output, 16);
+    memcpy(data_md5 + 16, data, 24);
+    mbedtls_md5_init(&md5_ctx);
+    mbedtls_md5_update(&md5_ctx, data_md5, sizeof(data_md5));
+    mbedtls_md5_finish(&md5_ctx, md5Output);
+    // Copy the next 16 bytes to the output buffer
+    memcpy(outputBuffer + i, md5Output, 16);
+  }
+
+  mbedtls_md5_free(&md5_ctx);
+}
+
